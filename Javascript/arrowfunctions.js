b1b1e2a0d8d09e8a767420d13e1a7a03f363f4a6
webdevtwo <butakikeith@gmail.com>
2024-07-05 15:52:57 +0300

    I have added three lines of code

--- Javascript/arrowfunctions.js
+++ Javascript/arrowfunctions.js
@@ -1,1 +1,7 @@
 const arrowFunctions = () => {}
+// an arrow function to add two numbers
+const addNumbers = (a, b) => a + b;
+
+// call the function with two numbers
+const result = addNumbers(5, 3);
+console.log(result)
