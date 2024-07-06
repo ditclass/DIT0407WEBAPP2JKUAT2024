01d0c09e46f904eaf7a2bed0ff20e59e3ceea893
Ninsima14 <ninsimawangui@gmail.com>
2024-07-06 18:36:11 +0300

    arrow function  added

--- Javascript/arrowfunctions.js
+++ Javascript/arrowfunctions.js
@@ -1,2 +1,5 @@
 const arrowFunctions = () => {}
 const greetings = name => { console. log(`Hello, ${name}!`); }; greetings('John'); // Hello, John!
+hello = function() {
+    return "Hello World!";
+  }
